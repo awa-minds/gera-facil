c9002e9bfbc23bbb4e8933ff92c5a78704cc3720
Alley M. Carvalho <alleycarvalho@gmail.com>
2023-05-16 15:42:29 -0300

    chore: add gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  assetPrefix: '/gera-facil/',
+  basePath: '/gera-facil',
+  output: 'export',
+  trailingSlash: true,
+}
 
 module.exports = nextConfig
