623d03e9ae3b82cd4dbc8d926baed0526f188877
Wesley Eufrasio <wesley.eufrasio@cmsw.com>
2023-05-31 12:40:49 -0300

    feat: inclusão das páginas de privacidade e termos de uso
    inclusão das páginas de privacidade e termos de uso

--- src/app/politicas-de-privacidade/page.tsx
+++ src/app/politicas-de-privacidade/page.tsx
@@ -7,47 +7,70 @@ export default function PoliticasDePrivacidadePage() {
   return (
     <div className="page">
       <h1>Políticas de Privacidade</h1>
+      <br></br>
+      <p>
+        O GeraFácil valoriza a privacidade dos seus usuários e, por isso, adota
+        medidas técnicas e organizacionais para proteger as informações pessoais
+        fornecidas pelos usuários. Esta Política de Privacidade explica como as
+        informações pessoais são coletadas, armazenadas, processadas e usadas
+        pelo GeraFácil. Ao utilizar o site do GeraFácil, você concorda com esta
+        Política de Privacidade.
+      </p>
 
       <div className="page-content">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          consectetur rhoncus lacus vitae lacinia. Suspendisse congue nibh sed
-          tempor lacinia. Aliquam erat volutpat. Nunc vestibulum eleifend
-          mollis. Mauris a ante a sem porttitor ultricies non interdum urna.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean
-          sit amet massa vel justo maximus pretium eget sed ipsum. Maecenas
-          fermentum egestas enim quis sagittis.
-        </p>
-        <p>
-          Maecenas fermentum finibus dapibus. Quisque et aliquet metus. Etiam
-          turpis metus, dignissim sed ante non, posuere lacinia enim. Phasellus
-          vitae hendrerit risus, sit amet fermentum ipsum. Integer nibh lorem,
-          convallis hendrerit aliquam sit amet, pulvinar eu purus. Praesent
-          egestas scelerisque libero id varius. Donec pharetra ultricies
-          scelerisque. Suspendisse dignissim a ipsum quis pretium. Suspendisse
-          consectetur nibh diam. Fusce ligula libero, sollicitudin sit amet
-          fringilla non, facilisis sit amet lacus. Nunc ultricies velit urna, eu
-          auctor eros luctus at. Morbi eleifend mauris sem, id viverra lorem
-          varius vitae.
-        </p>
-        <p>
-          Orci varius natoque penatibus et magnis dis parturient montes,
-          nascetur ridiculus mus. Nunc et purus sit amet justo viverra aliquet
-          quis auctor lectus. Cras porta lectus vel laoreet blandit. Praesent
-          dignissim gravida egestas. Curabitur vestibulum enim a consequat
-          lacinia. Mauris sit amet placerat lectus, consequat vestibulum eros.
-          Duis at dolor egestas, commodo ex id, fringilla augue. Maecenas ante
-          lorem, porttitor vitae vestibulum sit amet, lobortis eu velit.
-          Pellentesque imperdiet tortor erat, non efficitur arcu pellentesque
-          vitae.
-        </p>
-        <p>
-          Sed ornare facilisis dolor, nec aliquet ex interdum et. In sed semper
-          odio. Maecenas rhoncus molestie commodo. Donec eget nunc venenatis
-          turpis gravida euismod sit amet vitae ligula. Nulla eu auctor elit.
-          Etiam tempor fermentum tempor. Cras consectetur odio nec nisl varius,
-          non facilisis nisi rhoncus.
-        </p>
+        <ul>
+          <li>1. Coleta de informações pessoais</li>
+          <p>
+            O GeraFácil não armazena nenhum dado em nossos servidores. Isso
+            significa que não coletamos, armazenamos ou processamos informações
+            pessoais de usuários do GeraFácil. No entanto, o GeraFácil pode
+            coletar informações não pessoais de usuários, como o tipo de
+            navegador usado, o endereço IP e as páginas visitadas.
+          </p>
+          <br></br>
+          <li>2. Uso de cookies</li>
+          <p>
+            O GeraFácil pode usar cookies para melhorar a experiência do usuário
+            e fornecer conteúdo personalizado. Um cookie é um arquivo de texto
+            que é armazenado no computador do usuário para lembrar suas
+            preferências e informações de login. Os usuários podem optar por
+            desativar os cookies nas configurações do navegador, mas isso pode
+            afetar a funcionalidade do site.
+          </p>
+          <br></br>
+          <li>3. Compartilhamento de informações pessoais</li>
+          <p>
+            O GeraFácil não compartilha informações pessoais de usuários com
+            terceiros. No entanto, o GeraFácil pode compartilhar informações não
+            pessoais de usuários, como estatísticas de uso, com terceiros para
+            fins de análise e melhoria do serviço.
+          </p>
+          <br></br>
+          <li>4. Segurança das informações pessoais</li>
+          <p>
+            O GeraFácil adota medidas de segurança para proteger as informações
+            pessoais fornecidas pelos usuários. No entanto, a transmissão de
+            informações pela Internet não é totalmente segura e o GeraFácil não
+            pode garantir a segurança das informações transmitidas pelos
+            usuários.
+          </p>
+          <br></br>
+          <li>5. Mudanças nesta Política de Privacidade</li>
+          <p>
+            O GeraFácil pode atualizar esta Política de Privacidade
+            periodicamente. A versão atualizada será publicada no site do
+            GeraFácil. Os usuários são incentivados a revisar esta Política de
+            Privacidade regularmente.
+          </p>
+          <br></br>
+          <li>6. Contato</li>
+          <p>
+            Se você tiver alguma dúvida ou preocupação sobre esta Política de
+            Privacidade ou sobre o processamento de informações pessoais pelo
+            GeraFácil, entre em contato conosco através da nossa página de
+            Contato.
+          </p>
+        </ul>
       </div>
     </div>
   )
--- src/app/termos-de-uso/page.tsx
+++ src/app/termos-de-uso/page.tsx
@@ -10,43 +10,34 @@ export default function TermosDeUsoPage() {
 
       <div className="page-content">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-          consectetur rhoncus lacus vitae lacinia. Suspendisse congue nibh sed
-          tempor lacinia. Aliquam erat volutpat. Nunc vestibulum eleifend
-          mollis. Mauris a ante a sem porttitor ultricies non interdum urna.
-          Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean
-          sit amet massa vel justo maximus pretium eget sed ipsum. Maecenas
-          fermentum egestas enim quis sagittis.
+          Ao acessar o GeraFácil, o usuário deve estar ciente e concordar com os
+          termos de uso estabelecidos neste documento. Caso não concorde com
+          estes termos, solicitamos que não utilize o site. O GeraFácil se
+          reserva ao direito de modificar estes termos a qualquer momento, sem
+          aviso prévio.
         </p>
+      </div>
+      <br></br>
+      <h1>Licença de Uso</h1>
+      <div className="page-content">
         <p>
-          Maecenas fermentum finibus dapibus. Quisque et aliquet metus. Etiam
-          turpis metus, dignissim sed ante non, posuere lacinia enim. Phasellus
-          vitae hendrerit risus, sit amet fermentum ipsum. Integer nibh lorem,
-          convallis hendrerit aliquam sit amet, pulvinar eu purus. Praesent
-          egestas scelerisque libero id varius. Donec pharetra ultricies
-          scelerisque. Suspendisse dignissim a ipsum quis pretium. Suspendisse
-          consectetur nibh diam. Fusce ligula libero, sollicitudin sit amet
-          fringilla non, facilisis sit amet lacus. Nunc ultricies velit urna, eu
-          auctor eros luctus at. Morbi eleifend mauris sem, id viverra lorem
-          varius vitae.
+          O GeraFácil concede uma licença de uso limitada aos usuários que
+          concordam e cumprem os termos de uso. O GeráFacil reserva-se o direito
+          de restringir o acesso ao site para qualquer usuário, a qualquer
+          momento e por qualquer motivo.
         </p>
+      </div>
+      <br></br>
+      <h1>Responsabilidade de Uso</h1>
+      <div className="page-content">
         <p>
-          Orci varius natoque penatibus et magnis dis parturient montes,
-          nascetur ridiculus mus. Nunc et purus sit amet justo viverra aliquet
-          quis auctor lectus. Cras porta lectus vel laoreet blandit. Praesent
-          dignissim gravida egestas. Curabitur vestibulum enim a consequat
-          lacinia. Mauris sit amet placerat lectus, consequat vestibulum eros.
-          Duis at dolor egestas, commodo ex id, fringilla augue. Maecenas ante
-          lorem, porttitor vitae vestibulum sit amet, lobortis eu velit.
-          Pellentesque imperdiet tortor erat, non efficitur arcu pellentesque
-          vitae.
-        </p>
-        <p>
-          Sed ornare facilisis dolor, nec aliquet ex interdum et. In sed semper
-          odio. Maecenas rhoncus molestie commodo. Donec eget nunc venenatis
-          turpis gravida euismod sit amet vitae ligula. Nulla eu auctor elit.
-          Etiam tempor fermentum tempor. Cras consectetur odio nec nisl varius,
-          non facilisis nisi rhoncus.
+          Todos os conteúdos apresentados neste site têm finalidade
+          exclusivamente informativa e não devem ser considerados completos ou
+          atualizados. Eles não substituem a orientação de um profissional, como
+          um advogado, médico ou financeiro. O GeraFácil não oferece garantias
+          de qualquer tipo sobre os conteúdos e não se responsabiliza pelo uso
+          que os usuários possam fazer deles. Qualquer risco decorrente da
+          utilização dos conteúdos é assumido pelo próprio usuário.
         </p>
       </div>
     </div>
