8285263c5f45ba37bcae8ea4f503fc671ba78b44
Wesley Eufrasio <wesley.eufrasio@cmsw.com>
2023-06-07 19:05:36 -0300

    feat: inclusão das páginas de geradores e validadores
    inclusão das páginas de geradores e validadores

--- src/app/geradores/page.tsx
+++ src/app/geradores/page.tsx
@@ -1,15 +1,32 @@
+import CardPlus from '@/components/CardPlus'
+
 export const metadata = {
-  title: 'Geradores | Gera Fácil',
-  description: '',
+  title: 'Geradores | GeraFácil',
+  description: 'Geradores',
 }
 
 export default function GeradoresPage() {
   return (
     <div className="page">
-      <h1>Geradores</h1>
+      <h1 className="flex-col">
+        Geradores | GeraFácil
+      </h1>
+      <h2>Abaixo estão todos os geradores disponíveis em nosso site para uso.</h2>
 
       <div className="page-content">
-        <p>Adicionar todos os geradores...</p>
+        <h3>Geradores</h3>
+        <div className="mt-2 grid gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <CardPlus href="/geradores/gerador-senha" title="Gerador de Senha" />
+          <CardPlus href="/geradores/gerador-cpf" title="Gerador de CPF" />
+          <CardPlus href="/geradores/gerador-cnh" title="Gerador de CNH" />
+          <CardPlus href="/geradores/gerador-cnpj" title="Gerador de CNPJ" />
+          <CardPlus href="/geradores/gerador-pis" title="Gerador de PIS" />
+          <CardPlus
+            href="/geradores/gerador-qrcode"
+            title="Gerador de QR Code"
+          />
+          <CardPlus href="/geradores/gerador-cpf" title="Gerador de CPF" />
+        </div>
       </div>
     </div>
   )
--- src/app/validadores/page.tsx
+++ src/app/validadores/page.tsx
@@ -1,15 +1,36 @@
+import CardPlus from '@/components/CardPlus'
+
 export const metadata = {
-  title: 'Validadores | Gera Fácil',
-  description: '',
+  title: 'Validadores | GeraFácil',
+  description: 'Validadores',
 }
 
 export default function ValidadoresPage() {
   return (
     <div className="page">
-      <h1>Validadores</h1>
+      <h1 className="flex-col">
+        Validadores | GeraFácil
+      </h1>
+      <h2>Abaixo estão todos os validadores disponíveis em nosso site para uso.</h2>
 
       <div className="page-content">
-        <p>Adicionar todos os validadores...</p>
+        <h3>Validadores</h3>
+        <div className="mt-2 grid gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <CardPlus
+            href="/validadores/validador-senha"
+            title="Validador de Senha"
+          />
+          <CardPlus
+            href="/validadores/validador-cpf"
+            title="Validador de CPF"
+          />
+          <CardPlus
+            href="/validadores/validador-cnh"
+            title="Validador de CNH"
+          />
+          <CardPlus href="/validadores/validador-pis" title="Validador de PIS" />
+          <CardPlus href="/validadores/validador-cnpj" title="Validador de CNPJ" />
+        </div>
       </div>
     </div>
   )
