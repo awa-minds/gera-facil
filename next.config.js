b2ae77f9f9e561771dc4a45045a5f021b3619d6a
Wesley Eufrasio <wesley.eufrasio@cmsw.com>
2023-06-30 11:39:06 -0300

    refactor: ajuste no base path tirando o /gera-facil
    ajuste no base path tirando o /gera-facil

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const environment = process.env.NODE_ENV
 const isProduction = environment === 'production'
-const basePath = isProduction ? '/gera-facil' : ''
+const basePath = isProduction ? '' : ''
 
 const nextConfig = {
   assetPrefix: basePath ? `${basePath}/` : '',
