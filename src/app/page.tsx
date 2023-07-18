ad2ac4477a5afbb8781964a86ee82ab6dd309f19
Wesley Eufrasio <wesley.eufrasio@cmsw.com>
2023-07-18 11:13:33 -0300

    test: teste do google verification
    teste do google verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,7 @@ export const metadata = {
   robots: 'index, follow',
   author: 'GeraFácil',
   canonical: 'https://www.gerafacil.com.br',
-  name: 'google-site-verification',
-  content: 'fy0_KcqyT2MEMz66F1ExYsigsIUSMm4FBG5iz2fngro',
+  'google-site-verification': 'fy0_KcqyT2MEMz66F1ExYsigsIUSMm4FBG5iz2fngro',
 }
 
 export default function HomePage() {
