d727d4131f565031c3a70fb2d51aa1528cfde421
wesleywre <wesley_wre@hotmail.com>
2024-03-21 23:03:49 -0300

    Ajuste formulario de contato, validador de password e qrcoe

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: isProduction,
+    domains: ['api.qrserver.com'],
   },
   output: isProduction ? 'export' : 'standalone',
   trailingSlash: true,
--- src/app/contato/Contact.tsx
+++ src/app/contato/Contact.tsx
@@ -1,89 +1,56 @@
 'use client'
 
-import axios from 'axios'
-import React, { useState, ChangeEvent, FormEvent } from 'react'
+import { useState } from 'react';
 
-export const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
+export function Contact() {
+  const [sentMessage, setSentMessage] = useState("");
+  const ACCESS_KEY = "42755748-5ad6-44b8-aa11-3a57d0c2ed8e"; // Acesso à chave de ambiente
 
-  // eslint-disable-next-line prettier/prettier
-  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = event.target
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }))
-  }
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    try {
-      const response = await axios.post('/api/contact', formData)
-      console.log(response.data) // Assuming the response from the backend will be logged
-      // You can display a success message or perform other actions on successful form submission
-    } catch (error) {
-      console.error('Error submitting form:', error)
-      // Handle error, display an error message, etc.
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        access_key: ACCESS_KEY, // Usando a chave de ambiente
+        name: e.target.name.value,
+        email: e.target.email.value,
+        subject: "GERAFACIL",
+        message: e.target.message.value,
+      }),
+    });
+    const result = await response.json();
+    if (result.success) {
+      setSentMessage("Mensagem enviada com sucesso!");
+      e.target.reset(); // Limpa os campos do formulário após o envio bem-sucedido
     }
   }
 
   return (
-    <form
-      className="mx-auto max-w-md rounded-lg border p-4 shadow-md"
-      onSubmit={handleSubmit}
-    >
-      <div className="mb-4">
-        <label htmlFor="name" className="mb-1 block font-medium">
-          Nome:
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-          className="w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="mb-1 block font-medium">
-          Email:
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-          className="w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="message" className="mb-1 block font-medium">
-          Mensagem:
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-          className="w-full resize-none rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
-      <button
-        type="submit"
-        className="w-full bg-awa-100 px-6 py-2 text-white transition-colors hover:bg-awa-300"
-      >
-        Enviar
-      </button>
-    </form>
-  )
+    <>
+      <form className="mx-auto max-w-md rounded-lg border p-4 shadow-md" onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="mb-1 block font-medium">Nome:</label>
+          <input type="text" name="name" required placeholder="Seu Nome" className="w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="email" className="mb-1 block font-medium">Email:</label>
+          <input type="email" name="email" required placeholder="email@example.com" className="w-full rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="message" className="mb-1 block font-medium">Mensagem:</label>
+          <textarea name="message" required placeholder="Digite a mensagem:" className="w-full resize-none rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+        </div>
+        <button type="submit" className="w-full bg-awa-100 px-6 py-2 text-white transition-colors hover:bg-awa-300">Enviar</button>
+      </form>
+      {sentMessage && (
+        <div className="bg-green-500 text-white px-4 py-2 fixed bottom-5 right-5 rounded-lg">
+          <p>{sentMessage}</p>
+        </div>
+      )}
+    </>
+  );
 }
