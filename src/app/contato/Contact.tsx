9f0138dbe4a808540ee42c0738f29a111b67a2df
wesleywre <wesley_wre@hotmail.com>
2024-03-22 17:09:53 -0300

    ajuste prettier

--- src/app/contato/Contact.tsx
+++ src/app/contato/Contact.tsx
@@ -71,10 +71,10 @@ export function Contact() {
         </div>
         <button
           type="submit"
-          className="w-full bg-awa-100 px-6 py-2 text-white transition-colors hover:bg-awa-300">
+          className="w-full bg-awa-100 px-6 py-2 text-white transition-colors hover:bg-awa-300"
+        >
           Enviar
         </button>
-
       </form>
       {sentMessage && (
         <div className="fixed bottom-5 right-5 rounded-lg bg-green-500 px-4 py-2 text-white">
