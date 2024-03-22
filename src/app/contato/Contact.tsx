f5779325e8ef5606bd89746784e3b9ab1775002b
wesleywre <wesley_wre@hotmail.com>
2024-03-22 16:53:20 -0300

    teste prettier

--- src/app/contato/Contact.tsx
+++ src/app/contato/Contact.tsx
@@ -73,8 +73,7 @@ export function Contact() {
             required
             placeholder="Digite a mensagem:"
             className="w-full resize-none rounded-md border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-          </textarea>
+          ></textarea>
         </div>
         <button
           type="submit"
@@ -89,4 +88,4 @@ export function Contact() {
       )}
     </>
   )
-}
+}  
